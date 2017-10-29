b67486c73bcbf24ebcbfcf2a5d96243665e153ba
home <13492467@qq.com>
2017-10-30 07:13:36 +0800

    fix

--- Project/F407Disc/RTE/_STM32F4-DISCO/RTE_Components.h
+++ Project/F407Disc/RTE/_STM32F4-DISCO/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'fft' 
+ * Project: 'audio' 
  * Target:  'STM32F4-DISCO' 
  */
 
